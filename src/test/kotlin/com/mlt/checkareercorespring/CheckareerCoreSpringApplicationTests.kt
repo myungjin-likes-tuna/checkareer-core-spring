aea81e90d51439a3722f3b9a4bbf5d4090c92ff9
YooInKeun <keun0390@naver.com>
2022-02-02 18:35:27 +0900

    feat: add initial commit 🔥

---
+++ src/test/kotlin/com/mlt/checkareercorespring/CheckareerCoreSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mlt.checkareercorespring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CheckareerCoreSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
