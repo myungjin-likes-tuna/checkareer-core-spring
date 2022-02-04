428f7823207d7e728fdabc29c1232ed4ff0f20d2
YooInKeun <keun0390@naver.com>
2022-02-04 23:11:32 +0900

    feat: add node relation creation api

--- src/main/kotlin/com/mlt/checkareercorespring/samplenode/application/SampleNodeService.kt
+++ src/main/kotlin/com/mlt/checkareercorespring/samplenode/application/SampleNodeService.kt
@@ -1,11 +1,14 @@
 package com.mlt.checkareercorespring.samplenode.application
 
+import com.mlt.checkareercorespring.samplenode.domain.SampleNode
 import com.mlt.checkareercorespring.samplenode.domain.SampleNodeRepository
+import com.mlt.checkareercorespring.samplenode.dto.RelationsSaveRequest
 import com.mlt.checkareercorespring.samplenode.dto.SampleNodeGetResponse
 import com.mlt.checkareercorespring.samplenode.dto.SampleNodeSaveRequest
 import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 @Service
 @RequiredArgsConstructor
@@ -14,25 +17,31 @@ class SampleNodeService(
 ) {
 
     @Transactional
-    fun createNode(sampleNodeSaveRequest: SampleNodeSaveRequest) {
-        sampleNodeRepository.save(sampleNodeSaveRequest.toSampleNode())
+    fun createNode(node: SampleNode) {
+        sampleNodeRepository.save(node)
     }
 
     @Transactional(readOnly = true)
-    fun getAllNodes(): List<SampleNodeGetResponse> {
-        return SampleNodeGetResponse.listOf(sampleNodeRepository.findAll())
+    fun getAllNodes(): List<SampleNode> {
+        return sampleNodeRepository.findAll()
     }
 
     @Transactional(readOnly = true)
-    fun getNode(nodeId: Long): SampleNodeGetResponse {
-        val node = sampleNodeRepository.findById(nodeId).orElseThrow {
+    fun getNode(nodeId: Long): SampleNode {
+        return sampleNodeRepository.findById(nodeId).orElseThrow {
             throw IllegalArgumentException("존재하지 않는 노드입니다. (nodeId: ${nodeId})")
         }
-        return SampleNodeGetResponse.of(node)
     }
 
     @Transactional
     fun deleteNode(nodeId: Long) {
         sampleNodeRepository.deleteById(nodeId)
+    }
+
+    @Transactional
+    fun createRelations(startNodeId: Long, targetNodeIds: Set<Long>) {
+        val startNode = getNode(startNodeId)
+        startNode.endNodes = targetNodeIds.map { getNode(it) }.toSet()
+        sampleNodeRepository.save(startNode)
     }
 }
--- src/main/kotlin/com/mlt/checkareercorespring/samplenode/presentation/SampleNodeController.kt
+++ src/main/kotlin/com/mlt/checkareercorespring/samplenode/presentation/SampleNodeController.kt
@@ -1,6 +1,7 @@
 package com.mlt.checkareercorespring.samplenode.presentation
 
 import com.mlt.checkareercorespring.samplenode.application.SampleNodeService
+import com.mlt.checkareercorespring.samplenode.dto.RelationsSaveRequest
 import com.mlt.checkareercorespring.samplenode.dto.SampleNodeGetResponse
 import com.mlt.checkareercorespring.samplenode.dto.SampleNodeSaveRequest
 import lombok.RequiredArgsConstructor
@@ -17,23 +18,32 @@ class SampleNodeController(
 
     @PostMapping("/nodes", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun createNode(@RequestBody sampleNodeSaveRequest: SampleNodeSaveRequest): ResponseEntity<Void> {
-        sampleNodeService.createNode(sampleNodeSaveRequest)
+        sampleNodeService.createNode(sampleNodeSaveRequest.toSampleNode())
         return ResponseEntity(HttpStatus.OK)
     }
 
     @GetMapping("/nodes", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getAllNodes(): ResponseEntity<List<SampleNodeGetResponse>> {
-        return ResponseEntity.ok(sampleNodeService.getAllNodes())
+        return ResponseEntity.ok(SampleNodeGetResponse.listOf(sampleNodeService.getAllNodes()))
     }
 
     @GetMapping("/nodes/{nodeId}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getNode(@PathVariable nodeId: Long): ResponseEntity<SampleNodeGetResponse> {
-        return ResponseEntity.ok(sampleNodeService.getNode(nodeId))
+        return ResponseEntity.ok(SampleNodeGetResponse.of(sampleNodeService.getNode(nodeId)))
     }
 
     @DeleteMapping("/nodes/{nodeId}")
     fun deleteNode(@PathVariable nodeId: Long): ResponseEntity<Void> {
         sampleNodeService.deleteNode(nodeId)
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @PostMapping("/nodes/relations/{startNodeId}", consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun createRelations(
+        @PathVariable startNodeId: Long,
+        @RequestBody relationSaveRequest: RelationsSaveRequest
+    ): ResponseEntity<Void> {
+        sampleNodeService.createRelations(startNodeId, relationSaveRequest.targetNodeIds)
         return ResponseEntity(HttpStatus.OK)
     }
 }
