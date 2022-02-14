9c4d4ceb1fbd2ac43895d4732af8c152fe131455
YooInKeun <keun0390@naver.com>
2022-02-15 00:03:27 +0900

    feat: add create user api

--- src/main/kotlin/com/mlt/checkareercorespring/user/application/UserService.kt
+++ src/main/kotlin/com/mlt/checkareercorespring/user/application/UserService.kt
@@ -1,16 +1,31 @@
 package com.mlt.checkareercorespring.user.application
 
 import com.mlt.checkareercorespring.common.Paging
+import com.mlt.checkareercorespring.skill.domain.SkillRepository
+import com.mlt.checkareercorespring.user.domain.User
 import com.mlt.checkareercorespring.user.domain.UserRepository
+import com.mlt.checkareercorespring.user.dto.UserSaveRequest
 import lombok.RequiredArgsConstructor
 import org.neo4j.driver.internal.value.MapValue
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 @RequiredArgsConstructor
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val skillRepository: SkillRepository
 ) {
+
+    @Transactional
+    fun createUser(userSaveRequest: UserSaveRequest) {
+        userRepository.findById(userSaveRequest.id)
+            .ifPresent { throw IllegalArgumentException("이미 존재하는 유저입니다. (userId: ${userSaveRequest.id})") }
+
+        val skills = skillRepository.findAllById(userSaveRequest.skills)
+        val user = User(userSaveRequest.id, userSaveRequest.name, skills)
+        userRepository.save(user)
+    }
 
     fun getUserSkillSimilarity(userId: Long, paging: Paging): List<MapValue> {
         return userRepository.findUserSkillSimilarityBySkillGraphOrderBySimilarityDesc(
