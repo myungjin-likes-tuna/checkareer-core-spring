1845daff2c4cb0a67cde730aaa674634897dc016
YooInKeun <keun0390@naver.com>
2022-02-15 00:30:58 +0900

    feat: add update user skills api

--- src/main/kotlin/com/mlt/checkareercorespring/user/presentation/UserController.kt
+++ src/main/kotlin/com/mlt/checkareercorespring/user/presentation/UserController.kt
@@ -4,6 +4,7 @@ import com.mlt.checkareercorespring.common.Paging
 import com.mlt.checkareercorespring.user.application.UserService
 import com.mlt.checkareercorespring.user.dto.UserSaveRequest
 import com.mlt.checkareercorespring.user.dto.UserSkillSimilarityGetResponse
+import com.mlt.checkareercorespring.user.dto.UserSkillUpdateRequest
 import lombok.RequiredArgsConstructor
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -11,19 +12,26 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+// TODO: 예외 상황에 따라 response 규격 정하기
 @RestController
 @RequiredArgsConstructor
 class UserController(
     private val userService: UserService
 ) {
 
-    // TODO: 예외 상황에 따라 response 규격 정하기
     @PostMapping("/users", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun createUser(@RequestBody userSaveRequest: UserSaveRequest): ResponseEntity<Void> {
         userService.createUser(userSaveRequest)
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @PutMapping("/users/skills", consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun updateUserSkills(@RequestBody userSkillUpdateRequest: UserSkillUpdateRequest): ResponseEntity<Void> {
+        userService.updateUserSkills(userSkillUpdateRequest)
         return ResponseEntity(HttpStatus.OK)
     }
 
