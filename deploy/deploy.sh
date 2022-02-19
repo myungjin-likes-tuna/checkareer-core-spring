ce86c041b5810b62e6ee41b72783a36968d0d275
Deo <nexters@kakao.com>
2022-02-19 15:06:51 +0900

    ci(deploy): systemd 배포 추가

---
+++ deploy/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REPOSITORY_DIR=/home/ec2-user/app
+APP_NAME=checkareer-core-spring
+PID_PATH=$REPOSITORY_DIR/PID
+
+echo "change directory"
+
+cd $REPOSITORY_DIR/$APP_NAME
+
+echo "git pull"
+
+git pull
+
+echo "app build"
+
+./gradlew clean build
+
+echo "copy executable jar file"
+
+cp $REPOSITORY_DIR/$APP_NAME/build/libs/*.jar /var/www/checkareer-core/releases
+
+systemctl restart checkareer-core
+systemctl status checkareer-core
