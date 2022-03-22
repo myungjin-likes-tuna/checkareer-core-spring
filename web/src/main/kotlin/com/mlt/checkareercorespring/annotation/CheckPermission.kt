053045027fe2da5c8302c418204b671e03ad9738
kakao <aiden.yoo@kakaocorp.com>
2022-03-23 00:15:06 +0900

    chore: add permission check interceptor

---
+++ web/src/main/kotlin/com/mlt/checkareercorespring/annotation/CheckPermission.kt
@@ -0,0 +1,5 @@
+package com.mlt.checkareercorespring.annotation
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class CheckPermission
