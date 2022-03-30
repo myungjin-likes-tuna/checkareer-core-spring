9ca78fdd051ab1628af3884960e55e806489baa9
kakao <aiden.yoo@kakaocorp.com>
2022-03-31 00:35:16 +0900

    fix: 빌드 결과로 생성된 jar 파일명과 service에 등록된 jar 파일명 안 맞는 문제 임시 해결

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -19,6 +19,7 @@ echo "app build"
 echo "copy executable jar file"
 
 cp $REPOSITORY_DIR/$APP_NAME/build/libs/*.jar /var/www/checkareer-core/releases
+mv /var/www/checkareer-core/releases/web-0.0.1-SNAPSHOT.jar /var/www/checkareer-core/releases/checkareer-core-spring-0.0.1-SNAPSHOT.jar
 
 systemctl restart checkareer-core
 systemctl status checkareer-core
